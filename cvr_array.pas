8eb70f4f241e72a2f269c74fe79854db2fd4e60d
watemus <watemus@gmail.com>
2017-02-03 17:04:04 +0300

    Init procedures of GActorArray

--- cvr_array.pas
+++ cvr_array.pas
@@ -15,7 +15,6 @@ type
       arr: array of T;
       length: Integer;
       constructor create();
-      destructor destroy();
       procedure add(item: T);
       procedure destroyLast();
       procedure destroyAtIndex(index: Integer);
@@ -29,37 +28,50 @@ implementation
 
 constructor GActorArray.create;
 begin
-
-end;
-
-destructor GActorArray.destroy;
-begin
-
+  length := 0;
 end;
 
 procedure GActorArray.add(item: T);
 begin
-
+  inc(length);
+  SetLength(arr, length);
+  arr[length] := item;
 end;
 
 procedure GActorArray.destroyLast;
 begin
-
+  dec(length);
+  SetLength(arr, length);
 end;
 
 procedure GActorArray.destroyAtIndex(index: Integer);
+var
+  i: Integer;
 begin
-
+  if((index <= length) and (index >= 0)) then
+  begin
+    dec(length);
+    for i := index to length do
+    begin
+      arr[i] := arr[i+1];
+    end;
+    SetLength(arr, length);
+  end;
 end;
 
 function GActorArray.getItemAtIndex(index: Integer): T;
 begin
-
+  if((index <= length) and (index >= 0)) then
+  begin
+    getItemAtIndex := arr[index];
+    exit();
+  end;
+  getItemAtIndex := arr[0];
 end;
 
 function GActorArray.getLastItem: T;
 begin
-
+  getLastItem := arr[length];
 end;
 
 end.
